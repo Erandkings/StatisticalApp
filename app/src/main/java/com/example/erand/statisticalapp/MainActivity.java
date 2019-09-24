eb32496a7c392730a51e459df3804eb006df438c
Erandkings <erandkings12@gmail.com>
2019-09-24 20:08:12 +0100

    edits

--- app/src/main/java/com/example/erand/statisticalapp/MainActivity.java
+++ app/src/main/java/com/example/erand/statisticalapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
@@ -13,7 +14,7 @@ import com.github.mikephil.charting.utils.ColorTemplate;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    public static int count;
+//    public static int count;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,13 +24,15 @@ public class MainActivity extends AppCompatActivity {
         PieChart pieChart = (PieChart) findViewById(R.id.pie_chart);
 
         ArrayList<PieEntry> arrayList = new ArrayList<>();
-        arrayList.add(new PieEntry(12f));
+        arrayList.add(new PieEntry(6));
 
-        PieDataSet pieDataSet = new PieDataSet(getData(), "Result of your activities for this year");
+        PieDataSet pieDataSet = new PieDataSet(getData(), "");
         pieDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
 
         Description desc = new Description();
         desc.setText("Goal Tracker App");
+        desc.setTextSize(30);
+        desc.setPosition(400, 60);
 
 //        SimpleDateFormat time = new SimpleDateFormat("HH:mm");
 //        String currentTime = time.format(new Date());
@@ -42,35 +45,53 @@ public class MainActivity extends AppCompatActivity {
 
 //        Date getCurrentTime = Calendar.getInstance().getTime();
 
-
-
         PieData pieData = new PieData(pieDataSet);
         pieChart.setData(pieData);
         pieChart.setDescription(desc);
-        pieChart.setRotationEnabled(true);
+        pieChart.setRotationEnabled(false);
         pieChart.setHoleRadius(25f);
         pieChart.setTransparentCircleAlpha(0);
-        pieChart.setCenterText("Your Goal for this week");
+        pieChart.setCenterText("2019 Goals");
         pieChart.setCenterTextSize(10);
         pieChart.animateXY(5000, 5000);
         pieChart.invalidate();
 
+
     }
 
     private ArrayList getData() {
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int count = 0; count < 500; count++) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            new Entry();
+                        }
+                    });
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                    }
+                }
+            }
+        });
+
         ArrayList<PieEntry> entries = new ArrayList<>();
         entries.add(new PieEntry(11, "Jan"));
         entries.add(new PieEntry(12, "Feb"));
         entries.add(new PieEntry(13, "Mar"));
         entries.add(new PieEntry(14, "April"));
         entries.add(new PieEntry(15, "May"));
-        entries.add(new PieEntry(16, "June"));
-        entries.add(new PieEntry(17, "July"));
-        entries.add(new PieEntry(18, "August"));
-        entries.add(new PieEntry(19, "September"));
-        entries.add(new PieEntry(20, "October"));
-        entries.add(new PieEntry(21, "November"));
-        entries.add(new PieEntry(22, "December"));
+//        entries.add(new PieEntry(16, "June"));
+//        entries.add(new PieEntry(17, "July"));
+//        entries.add(new PieEntry(18, "August"));
+//        entries.add(new PieEntry(19, "September"));
+//        entries.add(new PieEntry(20, "October"));
+//        entries.add(new PieEntry(21, "November"));
+//        entries.add(new PieEntry(22, "December"));
         return entries;
     }
 }
--- app/src/main/java/com/example/erand/statisticalapp/ToDo.java
+++ app/src/main/java/com/example/erand/statisticalapp/ToDo.java
@@ -1,6 +1,7 @@
 package com.example.erand.statisticalapp;
 
 import android.app.DialogFragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -13,12 +14,21 @@ public class ToDo extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_to_do);
 
+        Button button1 = (Button) findViewById(R.id.check);
         Button button = (Button) findViewById(R.id.click);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 DialogFragment dialog = new TimePickerFragment();
                 dialog.show(getFragmentManager(), "TimePicker");
+            }
+        });
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
             }
         });
     }
